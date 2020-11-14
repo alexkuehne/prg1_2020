5cdd55c08b1a9ee11ae1a5a04efa34d30f47dc19
Alexander Kühne <alexander.kuehne@edu.teko.ch>
2020-11-14 08:16:14 +0100

    .

---
+++ src/ch/teko/prg/tag01/Work_Alex/Main.java
@@ -0,0 +1,13 @@
+package ch.teko.prg.tag01.Work_Alex;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter username: ");
+        String userName = myObj.nextLine();
+        System.out.println("Username is: " + userName);
+    }
+}
